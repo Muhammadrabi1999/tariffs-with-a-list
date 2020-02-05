f03b8e3b8d862536ee6eac983986c2de574538a5
Muhammadrabi1999 <muhammadrabi.shafizoda@bk.ru>
2020-02-05 12:09:43 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tariff1 from './components/Tariff/Tariff1';
+
+function App() {
+  return (
+    <Tariff1 />
+  );
+}
+
+export default App;
