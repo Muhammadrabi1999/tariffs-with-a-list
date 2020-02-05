c84c0838a952cb1c99fbe516db72215dd4fcc8ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-05 07:34:03 +0000

    Deploy Muhammadrabi1999/tariffs-with-a-list to github.com/Muhammadrabi1999/tariffs-with-a-list.git:gh-pages

---
+++ precache-manifest.9a042ba7e66c7945ce75a4b0688676d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e919c5c5579e82c3b6b0362e793dd05d",
+    "url": "/tariffs-with-a-list/index.html"
+  },
+  {
+    "revision": "220f5a2d240c8be92ee5",
+    "url": "/tariffs-with-a-list/static/css/main.948c31eb.chunk.css"
+  },
+  {
+    "revision": "baa9957001703ad007de",
+    "url": "/tariffs-with-a-list/static/js/2.34a18d9e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tariffs-with-a-list/static/js/2.34a18d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220f5a2d240c8be92ee5",
+    "url": "/tariffs-with-a-list/static/js/main.bf864f05.chunk.js"
+  },
+  {
+    "revision": "75918724438be92c5306",
+    "url": "/tariffs-with-a-list/static/js/runtime-main.a05bbbd4.js"
+  }
+]);
